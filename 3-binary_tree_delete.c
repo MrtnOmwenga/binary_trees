0d8c971755baded5153ee6399d4a37081afa2899
MrtnOmwenga <martin36449@gmail.com>
2023-03-21 09:03:54 -0700

    Betty coding style

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,19 +1,20 @@
 #include "binary_trees.h"
 
 /**
- *binary_delete_tree- Deletes whole tree
+ *binary_tree_delete- Deletes whole tree
  *
  *@tree: Tree to delete
  */
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-  if (tree == NULL){
-    return;
-  }
+if (tree == NULL)
+{
+return;
+}
 
-  binary_tree_delete(tree->left);
-  binary_tree_delete(tree->right);
+binary_tree_delete(tree->left);
+binary_tree_delete(tree->right);
 
-  free(tree);
+free(tree);
 }
