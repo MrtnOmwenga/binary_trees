7443f20175bd83862bb65c4ec8d46fea38a998d2
MrtnOmwenga <martin36449@gmail.com>
2023-03-21 09:44:38 -0700

    In-order and Pre-order transversal

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,21 +1,23 @@
+#include <stdlib.h>
 #include "binary_trees.h"
 
 /**
- * binary_tree_inorder - function that goes through a binary tree
- * using in-order traversal
+ * binary_tree_preorder - function that goes through a binary tree
+ * using pre-order traversal
  *
- * @tree:  pointer to the root node of the tree to traverse
- * @func: pointer to a function to call for each node. The
- * value in the node must be passed as a parameter to this function.
+ * @tree: pointer to the root node of the tree to traverse
+ * @func: pointer to a function to call for each node
+ * the value in the node must be passed as a param to this function
  *
- * Return: Nothing. if tree/func is NULL, do nothing
+ * Return: nothing
+ * if tree or func is null do nothing
  */
-void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (func == NULL || tree == NULL)
+	if (tree == NULL || func == NULL)
 		return;
 
-	binary_tree_inorder(tree->left, func);
 	func(tree->n);
-	binary_tree_inorder(tree->right, func);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
